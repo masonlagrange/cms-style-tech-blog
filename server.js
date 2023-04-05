d43522255dddfb993ae2b57d510de864cd5c3cc9
MasonLagrange <mason.lagrange@gmail.com>
2023-04-05 17:23:51 -0400

    bugfix

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store)
 
 
 const app = express();
-const PORT = process.env.JAWSDB_URL || 3001;
+const PORT = process.env.PORT || 3001;
 
 const sess = {
   secret: 'Super secret secret',
